9356071e8fbadbd5a7e75c782a2dfa68ea540d66
larysson <larysson.alves@gmail.com>
2021-09-12 17:06:04 -0300

    modificado node_env no docker-compose, extraido configuracao do typeorm para o app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './configs/typeorm.config';
 import { AlunoModule } from './aluno/aluno.module';
+import { Aluno } from './aluno/entities/aluno.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
+      playground: true,
     }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL || 'postgres://postgres:laa0609@localhost:5432/alunos',
+      entities: [Aluno],
+      synchronize: true,
+    }),
     AlunoModule,
   ],
 })
